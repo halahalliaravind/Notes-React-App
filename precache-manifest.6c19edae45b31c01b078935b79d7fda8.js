7b76e13fd39e78b9e1dc8b943c413629d9fc2515
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-07-16 19:16:34 +0530

    Updates

---
+++ precache-manifest.6c19edae45b31c01b078935b79d7fda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4385d9ecd12b41fecd5b52ad81a53773",
+    "url": "/Notes-React-App/index.html"
+  },
+  {
+    "revision": "e71a0ae1164f7ff9a9ba",
+    "url": "/Notes-React-App/static/css/main.a5804f80.chunk.css"
+  },
+  {
+    "revision": "f5c3505ec8f584c6b3c4",
+    "url": "/Notes-React-App/static/js/2.b301b908.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/Notes-React-App/static/js/2.b301b908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a0ae1164f7ff9a9ba",
+    "url": "/Notes-React-App/static/js/main.59aab6a3.chunk.js"
+  },
+  {
+    "revision": "c235a6e78d1c4e8d5227",
+    "url": "/Notes-React-App/static/js/runtime-main.2f281210.js"
+  }
+]);
